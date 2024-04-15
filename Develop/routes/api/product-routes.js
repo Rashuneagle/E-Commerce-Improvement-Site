8284b4be93edd6e249aa143b2eb31a4c29356d4e
Michael Scott <mscotteagle21@gmail.com>
2024-04-14 21:19:19 -0500

    seeded database and completed operations for routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -39,18 +39,55 @@ router.get('/:id', async (req, res) => {
 });
 
 // Create a new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // Implementation for creating a new product
+  try {
+    const newProduct = await Product.create({
+      ...req.body,
+    });
+    res.json(newProduct);
+  } catch (error) {
+    res.json(500).json({ error: 'Failed to create category' });
+  }
 });
 
 // Update a product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // Implementation for updating a product
+  try {
+    const [updatedCount, updatedProduct] = await Product.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+    });
+    if (updatedCount === 0) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+    res.json(updatedProduct[0]);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update category' })
+  }
 });
 
 // Delete a product
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // Implementation for deleting a product
+  try {
+    const newProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      },
+  });
+  
+    if (!newProduct) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
